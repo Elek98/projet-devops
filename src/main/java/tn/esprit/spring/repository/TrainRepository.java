14dd103201e03a8688798f109d0482810a8613c8
Ahmed Ben Romdhane <73753472+Elek98@users.noreply.github.com>
2022-12-13 08:34:37 +0100

    Update TrainRepository.java

--- src/main/java/tn/esprit/spring/repository/TrainRepository.java
+++ src/main/java/tn/esprit/spring/repository/TrainRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.Train;
-import tn.esprit.spring.entities.Voyage;
+
 
 
 
